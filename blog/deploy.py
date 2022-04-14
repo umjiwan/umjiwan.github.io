0ccb7492400bd8816ef53b519b593d59b4c54a62
ulhangry <80541020+umjiwan@users.noreply.github.com>
2022-04-14 21:08:05 +0900

    중간점검 3

--- blog/deploy.py
+++ blog/deploy.py
@@ -8,13 +8,13 @@ title = 0
 subtitle = 0
 date = 0
 hidden = 0
-# ---------------
 
 # 기존 파일 제거
 rm_list = os.listdir("post/")
 for rm in rm_list:
     os.remove("post/" + rm)
 os.remove("index.html")
+f = open("index.html", "w"); f.close()
 
 # html 파일 생성
 for post in post_list:
@@ -32,8 +32,8 @@ for post in post_list:
             option[1] = option[1][1:]
         globals()[option[0]] = option[1]
 
-    if bool(hidden):
-        pass
+    if hidden == "true":
+        continue
     
     content = post_content[post_content.find("}") + 1:]
     content_list = []
@@ -50,13 +50,67 @@ for post in post_list:
     content = "".join(content_list)
     title = title.replace(" ", "_")
 
+    with open("template/list.html", "r") as file:
+        index_content = file.read()
+
+    index_content = index_content.replace("{title}", title)
+    index_content = index_content.replace("{subtitle}", subtitle)
+    index_content = index_content.replace("{date}", date)
+
+    
+    """# {date} 문자열을 date의 값으로 바꾸기
+    index_date = index_content.find("{date}")
+    while index_date != -1:
+        index_content_list = []
+        for i in index_content:
+            index_content_list.append(i)
+        index_content_list[index_date:index_date+len("{date}")] = ""
+        for i in date:
+            index_content_list.insert(index_date, i)
+            index_date += 1
+        index_content = "".join(index_content_list)
+        index_date = index_content.find("{date}")
+
+    # {title} 문자열을 title의 값으로 바꾸기
+    index_title = index_content.find("{title}")
+    while index_title != -1:
+        index_content_list = []
+        for i in index_content:
+            index_content_list.append(i)
+        index_content_list[index_title:index_title+len("{title}")] = ""
+        for i in title:
+            index_content_list.insert(index_title, i)
+            index_title += 1
+        index_content = "".join(index_content_list)
+        index_title = index_content.find("{title}")
+
+    # {subtitle} 문자열을 title의 값으로 바꾸기
+    index_subtitle = index_content.find("{subtitle}")
+    while index_subtitle != -1:
+        index_content_list = []
+        for i in index_content:
+            index_content_list.append(i)
+        index_content_list[index_subtitle:index_subtitle+len("{subtitle}")] = ""
+        for i in subtitle:
+            index_content_list.insert(index_subtitle, i)
+            index_subtitle += 1
+        index_content = "".join(index_content_list)
+        index_subtitle = index_content.find("{subtitle}")
+        
+    
+    하... replace 있는거 까먹고 이렇게 짰다..
+    """
+
     with open("index.html", "a") as file:
-        file.write(f'<a href="post/{date}-{title}.html">{title}</a><br>\n')
+        file.write(index_content)
 
+    with open("template/post.html", "r") as file:
+        post_content = file.read()
+
+    content = post_content.replace("{content}", content)
+    
     with open(f"post/{date}-{title}.html", "w") as file:
-        file.write(f"{title}<br>{date}")
-        file.write(f"<p>{content}</p>")
-
+        file.write(content)
     
 
     
