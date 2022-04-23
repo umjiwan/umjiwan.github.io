6b96650227bef60a7dfa39e465983af7a5c108ba
ulhangry <80541020+umjiwan@users.noreply.github.com>
2022-04-24 00:02:15 +0900

    태그 기능 구현 👏

--- blog/deploy.py
+++ blog/deploy.py
@@ -1,46 +1,194 @@
 import os
 import re
 
-# vsc 오류 제거용---
-title = 0
-subtitle = 0
-date = 0
-hidden = 0
-tags = 0
-index = ""
+def getFileContent(fileName):
+    with open(f"md/{fileName}", "r") as file:
+        content = file.read()
+    return content
 
-# post_list 정렬
-post_list = os.listdir("md/")
-post_date_list = []
-for post in post_list:
-    with open("md/" + post, "r") as file:
-        post_content = file.read()
+def getPostContent(fileContent):
+    content = fileContent[fileContent.find("}") + 1:]
+    content_list = []
     
-    option_find = re.compile("[{].*[}]", re.DOTALL)
-    option_list = option_find.findall(post_content)
-    option_list = option_list[0][option_list[0].find("{")+1:option_list[0].find("}")-1]
-    option_list = option_list.split("\n")[1:]
+    for i in content:
+        content_list.append(i)
 
-    for option in option_list: # option 추출
-        option = option.split(":")
-        if option[1][0] == " ":
-            option[1] = option[1][1:]
-        globals()[option[0]] = option[1]
+    for i in range(len(content_list)):
+        if content_list[i] == "\n":
+            content_list[i] = ""
+        else:
+            break
+    
+    content = "".join(content_list)
 
-    post_date_list.append(f"{int(date.replace('-', ''))}-{post}")
-post_date_list.sort(reverse=True)
-post_list = []
-for post in post_date_list:
-    post_list.append(post.split("-")[1])
+    return content
 
-# 기존 파일 제거
-rm_list = os.listdir("post/")
-for rm in rm_list:
-    os.remove("post/" + rm)
-os.remove("index.html")
-f = open("index.html", "w"); f.close()
+def getOption(content):
+    optionFind = re.compile("[{].*[}]", re.DOTALL)
+    optionList = optionFind.findall(content)
+    optionList = optionList[0][optionList[0].find("{")+1:optionList[0].find("}")-1]
+    optionList = optionList.split("\n")[1:]
 
-# html 파일 생성
+    option = {}
+    for optionVar in optionList:
+        optionVar = optionVar.split(":")
+        if optionVar[1][0] == " ":
+            optionVar[1] = optionVar[1][1:]
+        option[optionVar[0]] = optionVar[1]
+
+    option["tags"] = option["tags"].replace(" ", "")
+    option["tags"] = option["tags"].split(",")
+
+    return option
+
+def getPostList():
+    postList = os.listdir("md/")
+    postDateList = []
+
+    for post in postList:
+        fileContent = getFileContent(post)
+        option = getOption(fileContent)
+
+        postDateList.append(f"{option['date'].replace('-', '')}-{post}")
+
+    postDateList.sort(reverse=True)
+    postList = []
+    for post in postDateList:
+        postList.append(post.split("-")[1])
+
+    return postList
+
+def removeFile(library):
+    removeList = os.listdir(library)
+    for remove in removeList:
+        os.remove(f"{library}/{remove}")
+
+def getListTemplate(option, post, dd=False):
+    with open("template/list.html", "r") as file:
+        listTemplate = file.read()
+    
+    listTemplate = listTemplate.replace("{title}", option["title"])
+    listTemplate = listTemplate.replace("{subtitle}", option["subtitle"])
+    listTemplate = listTemplate.replace("{date}", option["date"])
+
+    postName = post.replace(".md", "")
+    listTemplate = listTemplate.replace("{post_name}", postName)
+
+    if dd:
+        dd = "../"
+    else:
+        dd = ""
+
+    tagList = getTagList(option, f"{dd}tag/")
+    listTemplate = listTemplate.replace("{tags}", tagList)
+
+    if dd:
+        listTemplate = listTemplate.replace("{..}", "../")
+    else:
+        listTemplate = listTemplate.replace("{..}", "")
+
+    return listTemplate
+
+def getTagList(option, route):
+    tagList = []
+    for tag in option["tags"]:
+        tagList.append(f"<a class='tag' href='{route}{tag}.html'>{tag}</a>")
+    tagList = ", ".join(tagList)
+
+    return tagList
+
+def createIndex(listTemplate, route="index.html", dd=False):
+    with open("template/_index.html", "r") as file:
+        indexTemplate = file.read()
+    
+    if dd:
+        dd = "../"
+        tag = route.replace("tag/", "")
+        tag = tag.replace(".html", "")
+        indexTemplate = indexTemplate.replace("{dd}", f" - {tag}")
+    else:
+        dd = ""
+        indexTemplate = indexTemplate.replace("{dd}", "")
+        
+
+    indexTemplate = indexTemplate.replace("{list}", listTemplate)
+    indexTemplate = indexTemplate.replace("{..}", dd)
+
+    with open(route, "w") as file:
+        file.write(indexTemplate)
+
+def createPost(content, option, post):
+    with open("template/post.html", "r") as file:
+        postTemplate = file.read()
+    
+    postTemplate = postTemplate.replace("{title}", option["title"])
+    postTemplate = postTemplate.replace("{subtitle}", option["subtitle"])
+    postTemplate = postTemplate.replace("{date}", option["date"])
+    postTemplate = postTemplate.replace("{content}", content)
+
+    tagList = getTagList(option, "../tag/")
+
+    postTemplate = postTemplate.replace("{tags}", tagList)
+
+    with open(f"post/{option['date']}-{post.replace('.md', '')}.html", "w") as file:
+        file.write(postTemplate)
+
+def createTagFile(tagList, tagPostList):
+    tagList = list(set(tagList))
+    tagFileList = {}
+
+    for tag in tagList:
+        tagFile = []
+        for tagPost in tagPostList:
+            if tag in tagPostList[tagPost]:
+                tagFile.append(tagPost)
+        tagFileList[tag] = tagFile
+
+    for tag in tagFileList:
+        postList = tagFileList[tag]
+        listTemplate = ""
+
+        for post in postList:
+            fileContent = getFileContent(post)
+            option = getOption(fileContent)
+
+            listTemplate += getListTemplate(option, post, dd=True)
+        createIndex(listTemplate, route=f"tag/{tag}.html", dd=True)
+def main():
+    postList = getPostList()
+    listTemplate = ""
+    tagList = []
+    tagPostList = {}
+
+    removeFile("post/")
+
+    for post in postList:
+        fileContent = getFileContent(post)
+        content = getPostContent(fileContent)
+        option = getOption(fileContent)
+
+        if option["hidden"] == "true":
+            continue
+
+        listTemplate += getListTemplate(option, post)
+        createPost(content, option, post)
+
+        tagList += option["tags"]
+        tagPostList[post] = option["tags"]
+
+    if os.path.isfile("index.html"):
+        os.remove("index.html")
+
+    removeFile("tag/")
+
+    createIndex(listTemplate)
+    createTagFile(tagList, tagPostList)
+        
+if __name__ == "__main__":
+    main()
+    
+
+"""# html 파일 생성
 tag_list = []
 post_tag_list = []
 for post in post_list:
@@ -126,4 +274,4 @@ with open("template/_index.html", "r") as file:
 _index_content = _index_content.replace("{list}", index)
 
 with open("index.html", "w") as file:
-    file.write(_index_content)
+    file.write(_index_content)"""
